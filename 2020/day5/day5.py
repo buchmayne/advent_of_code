94ba66d78b1a30b5d542813c0a9a6b7c6f56beea
buchmayne <michaelmbuchman@gmail.com>
2020-12-05 16:46:38 -0800

    refactored

--- 2020/day5/day5.py
+++ 2020/day5/day5.py
@@ -3,49 +3,36 @@ data = f.read()
 list_of_boarding_passes = data.split("\n")
 
 
-def get_row_number(row_input):
-    total_rows = 128
-    first_row = 0
-    for x in row_input:
-        row_slicer = int((total_rows - first_row) / 2) + first_row
-        f_half = list(range(first_row, row_slicer))
-        b_half = list(range(row_slicer, total_rows))
-        if x == "F":
+def get_row_or_column_number(
+    data_input, f_input_type, b_input_type, min_input_type, max_input_type
+):
+    total_input_ = max_input_type
+    first_value = min_input_type
+    for x in data_input:
+        slicer_ = int((total_input_ - first_value) / 2) + first_value
+        f_half = list(range(first_value, slicer_))
+        b_half = list(range(slicer_, total_input_))
+        if x == f_input_type:
             if len(f_half) == 1:
                 return f_half[0]
-            total_rows = row_slicer
-            first_row = first_row
-        elif x == "B":
+            total_input_ = slicer_
+            first_value = first_value
+        elif x == b_input_type:
             if len(b_half) == 1:
                 return b_half[0]
-            total_rows = total_rows
-            first_row = row_slicer
-
-
-def get_column_number(column_input):
-    total_columns = 8
-    first_column = 0
-    for y in column_input:
-        column_slicer = int((total_columns - first_column) / 2) + first_column
-        f_half = list(range(first_column, column_slicer))
-        b_half = list(range(column_slicer, total_columns))
-        if y == "L":
-            if len(f_half) == 1:
-                return f_half[0]
-            total_columns = column_slicer
-            first_column = first_column
-        elif y == "R":
-            if len(b_half) == 1:
-                return b_half[0]
-            total_columns = total_columns
-            first_column = column_slicer
+            total_input_ = total_input_
+            first_value = slicer_
 
 
 def calculate_seat_id(boarding_pass):
     rows_ = boarding_pass[:7]
     columns_ = boarding_pass[7:]
-    row_id = get_row_number(rows_)
-    column_id = get_column_number(columns_)
+    row_id = get_row_or_column_number(
+        rows_, f_input_type="F", b_input_type="B", min_input_type=0, max_input_type=128
+    )
+    column_id = get_row_or_column_number(
+        columns_, f_input_type="L", b_input_type="R", min_input_type=0, max_input_type=8
+    )
     seat_id = (row_id * 8) + column_id
     return seat_id
 
@@ -53,8 +40,6 @@ def calculate_seat_id(boarding_pass):
 seat_ids = [
     calculate_seat_id(boarding_pass) for boarding_pass in list_of_boarding_passes
 ]
-
-print(max(seat_ids))
 
 
 # GET MISSING SEAT
@@ -68,4 +53,4 @@ for x in range(0, count_seats):
     if sorted_seat_ids[x] != continous_seats[x]:
         dont_match.append(sorted_seat_ids[x] - 1)
 
-print(min(dont_match))
+print("Highest Seat ID: {}\nMissing Seat ID: {}".format(max(seat_ids), min(dont_match)))
