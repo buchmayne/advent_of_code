96d40371734a7a7d7c29a6b0fa8d565aea999bfe
buchmayne <michaelmbuchman@gmail.com>
2020-12-10 14:28:43 -0800

    copied answer from youtube :( but learned about dynamic programming. its the friends we make along the way

--- 2020/day10/day10.py
+++ 2020/day10/day10.py
@@ -18,6 +18,24 @@ for i in list(range(len(data))):
 
 part1_answer = one_jolt_diff * three_jolt_diff
 
-print(part1_answer)
+# Part 2 // dynamic programming (thanks youtube)
+DP = {}
+data.append(max(data) + 3)
+data.append(0)
+data = sorted(data)
 
-# Part 2
+
+def dp(i):
+    if i == (len(data) - 1):
+        return 1
+    if i in DP:
+        return DP[i]
+    ans = 0
+    for j in range(i + 1, len(data)):
+        if data[j] - data[i] <= 3:
+            ans += dp(j)
+    DP[i] = ans
+    return ans
+
+
+print(dp(0))
